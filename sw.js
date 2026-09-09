6e1d9271583b7c24aec8c74ef51c144f39e368b8
thanakanondueng-rgb <thanakanondueng@gmail.com>
2026-09-09 15:05:04 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE="passit-v3";
-self.addEventListener("install",e=>{self.skipWaiting()});
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("notificationclick",e=>{e.notification.close();e.waitUntil(self.clients.matchAll({type:"window",includeUncontrolled:true}).then(cs=>cs[0]?.focus()||self.clients.openWindow("./")))});
